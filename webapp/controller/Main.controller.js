fa985a363e152d360d6f090a56bd7904be3584ce
okamotohy-sap <37536573+okamotohy-sap@users.noreply.github.com>
2018-03-19 15:53:52 +0100

    change main.controller.js

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,6 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-], function(Controller) {
+], function (Controller) {
 	"use strict";
-	return Controller.extend("public.controller.Main", {});
+	return Controller.extend("public.controller.Main", {
+		zoomToMap: function (oEvent) {
+			oEvent.getParameters().context = oEvent.getParameter("thing");
+			this.byId("idMap").doMapZoom(oEvent);
+		}
+	});
 });
