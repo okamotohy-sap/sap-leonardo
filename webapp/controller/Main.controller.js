666171a0facbc4338a5a95f4254183f4d958b558
okamotohy-sap <37536573+okamotohy-sap@users.noreply.github.com>
2018-03-19 16:40:27 +0100

    change thingpage

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -6,6 +6,17 @@ sap.ui.define([
 		zoomToMap: function (oEvent) {
 			oEvent.getParameters().context = oEvent.getParameter("thing");
 			this.byId("idMap").doMapZoom(oEvent);
+		},
+		oSingleHeaderSelect: function (oEvent) {
+			sap.ui.getCore().byId("idBusy").open();
+			var oThingObject = oEvent.getParameter("context").getParameters("thing").thingData;
+			this.getOwnerComponent().getRouter().navTo("thingpage", {
+				thingId: oThingObject.ThingId,
+				thingType: oThingObject.ThingType,
+				highSeverity: oThingObject.DYN_ENT_com_sap_appiot_eventtypes__StandardEventType.High || 0,
+				mediumSeverity: oThingObject.DYN_ENT_com_sap_appiot_eventtypes__StandardEventType.Medium || 0,
+				lowSeverity: oThingObject.DYN_ENT_com_sap_appiot_eventtypes__StandardEventType.Low || 0
+			});
 		}
 	});
 });
