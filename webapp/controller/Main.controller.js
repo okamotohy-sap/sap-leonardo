0eaf28827c4b82afbd4fa3f73616e356cc757f04
okamotohy-sap <37536573+okamotohy-sap@users.noreply.github.com>
2018-03-19 17:04:10 +0100

    modify main.controller.js

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -7,6 +7,22 @@ sap.ui.define([
 			oEvent.getParameters().context = oEvent.getParameter("thing");
 			this.byId("idMap").doMapZoom(oEvent);
 		},
+		oMultiRowSelect: function (oEvent) {
+			var oThingObject = oEvent.getParameter("context").getParameters("thing").thingData;
+			this.getOwnerComponent().getRouter().navTo("thingpage", {
+				thingId: oThingObject.ThingId,
+				thingType: oThingObject.ThingType,
+				highSeverity: oThingObject.DYN_ENT_com_sap_appiot_eventtypes__StandardEventType.High || 0,
+				mediumSeverity: oThingObject.DYN_ENT_com_sap_appiot_eventtypes__StandardEventType.Medium || 0,
+				lowSeverity: oThingObject.DYN_ENT_com_sap_appiot_eventtypes__StandardEventType.Low || 0
+			});
+		},
+
+		oMultiFooterSelect: function (oEvent) {
+			sap.ui.getCore().byId("idBusy").open();
+			this.getOwnerComponent().getRouter().navTo("thinglistpage", false);
+		},
+
 		oSingleHeaderSelect: function (oEvent) {
 			sap.ui.getCore().byId("idBusy").open();
 			var oThingObject = oEvent.getParameter("context").getParameters("thing").thingData;
@@ -17,6 +33,21 @@ sap.ui.define([
 				mediumSeverity: oThingObject.DYN_ENT_com_sap_appiot_eventtypes__StandardEventType.Medium || 0,
 				lowSeverity: oThingObject.DYN_ENT_com_sap_appiot_eventtypes__StandardEventType.Low || 0
 			});
+		},
+
+		oSingleFooterSelect: function (oEvent) {
+			sap.ui.getCore().byId("idBusy").open();
+			var oThingObject = oEvent.getParameter("context").getParameters("thing").thingData;
+			this.getOwnerComponent().getRouter().navTo("analysispage", {
+				thingId: oThingObject.ThingId,
+				headerTitle: oThingObject.ThingName,
+				subHeaderTitle: oThingObject.ThingExternalId
+			});
+		},
+
+		onAfterRendering: function () {
+			sap.ui.getCore().byId("idBusy").close();
 		}
+
 	});
 });
