36bc6444a866399e5a1551a333a7a5f465b7601a
okamotohy-sap <37536573+okamotohy-sap@users.noreply.github.com>
2018-03-22 14:49:44 +0100

    webapp\controller\ThingPage.controller.js

--- webapp/controller/ThingPage.controller.js
+++ webapp/controller/ThingPage.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/core/routing/History",
-  'sap/ui/model/json/JSONModel'
+  'sap/ui/model/json/JSONModel',
+  'sap/viz/ui5/format/ChartFormatter',
+
 ], function (Controller, formatter) {
   "use strict";
   return Controller.extend("webapp.controller.ThingPage", {
