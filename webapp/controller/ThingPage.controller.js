1809f2a849b307dba0439b74c310aa1584dcfb85
okamotohy-sap <37536573+okamotohy-sap@users.noreply.github.com>
2018-03-22 11:43:54 +0100

    ThingPage.controller.js

--- webapp/controller/ThingPage.controller.js
+++ webapp/controller/ThingPage.controller.js
@@ -10,7 +10,7 @@ sap.ui.define([
   			var oModel = new sap.ui.model.json.JSONModel();
   			this.getView().setModel(oModel, "thingPageModel");
   			oRouter.getRoute("thingpage").attachMatched(this._onRouteMatched, this);
-  		}
+  		},
       _onRouteMatched: function(oEvent) {}
       /**
   		/** Retreive the ThingId and ThingType and do a call to the backend with the expand paramaters to bind it to the header and basic data section **/
