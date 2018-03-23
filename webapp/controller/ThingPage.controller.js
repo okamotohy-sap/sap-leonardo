c231bd312ed4a91dc885b3a8391bfdd60543c751
okamotohy-sap <37536573+okamotohy-sap@users.noreply.github.com>
2018-03-23 12:24:46 +0100

    webapp\controller\ThingPage.controller.js

--- webapp/controller/ThingPage.controller.js
+++ webapp/controller/ThingPage.controller.js
@@ -58,7 +58,7 @@ sap.ui.define([
       var oModel = this.getView().getModel("thingPageModel");
       */
 
-      this.getView().setModel(oViewModel,"arg");
+      this.getView().setModel(arg,"arg");
 
       //this.aPath = arg.Property.split(".");
       //this.oChart.addDefaultPST(this.aPath[0], this.aPath[1]);
