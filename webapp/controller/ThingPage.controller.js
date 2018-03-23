a1d2ba849777d022f4bad89be24c622fc66206b4
okamotohy-sap <37536573+okamotohy-sap@users.noreply.github.com>
2018-03-23 15:57:28 +0100

    webapp\controller\ThingPage.controller.js

--- webapp/controller/ThingPage.controller.js
+++ webapp/controller/ThingPage.controller.js
@@ -57,10 +57,11 @@ sap.ui.define([
       var sHeaderTitle = arg.headerTitle;
       var sSubHeaderTitle = arg.subHeaderTitle;
       var oChart = this.byId("idChart");
+      /*
       oChart.setHeaderTitle("");
       oChart.setSubheaderTitle("");
       this.aPath = oEvent.getParameter("arguments").mpPath.split(".");
-
+      */
       /*
       oChart.setHeaderTitle("");
       oChart.setSubheaderTitle("");
