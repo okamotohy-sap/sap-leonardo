8c1190ec1dd8553e509a504bf7b7393a085f2570
okamotohy-sap <37536573+okamotohy-sap@users.noreply.github.com>
2018-03-26 12:20:35 +0200

    webapp\controller\ThingPage.controller.js

--- webapp/controller/ThingPage.controller.js
+++ webapp/controller/ThingPage.controller.js
@@ -62,6 +62,7 @@ sap.ui.define([
       oChart.setSubheaderTitle("");
       this.aPath = oEvent.getParameter("arguments").mpPath.split(".");
       */
+      this.eventsContext = sap.ui.getCore().getModel("eventsModel") && sap.ui.getCore().getModel("eventsModel").getData().eventsData;
       oChart.setHeaderTitle("");
       oChart.setSubheaderTitle("");
       oChart.addDefaultPST("TI_SensorTag_CAPPABILITY", "sensorAccX");
@@ -80,7 +81,6 @@ sap.ui.define([
       var oMpContext = oEvent.getParameter("context");
       oModel.setProperty("/mpData", oMpContext);
       */
-      this.eventsContext = sap.ui.getCore().getModel("eventsModel") && sap.ui.getCore().getModel("eventsModel").getData().eventsData;
 
       /*
       var oEventContext = oEvent.getParameter("event");
